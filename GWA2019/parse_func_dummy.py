cfab2992f3e169f33d230fb54131f4b49e0db6f3
seantyh <seantyh@gmail.com>
2019-03-28 23:17:44 +0800

    add word_pos_utils.py

---
+++ GWA2019/parse_func_dummy.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def is_classic(quote: List[str]) -> bool:
+    return False
+
+def get_examplar_words(example: List[str]) -> List[str]:
+    return []
+
+def query_as4_freq(word: str) -> int:
+    return 0
---
+++ GWA2019/word_pos_utils.py
@@ -0,0 +1,94 @@
+import json
+from . import parse_func_dummy as parse
+from itertools import chain
+
+class MOE:
+    def __init__(self, fpath):
+        self.moe_data = {}
+        self.load_moe_data(fpath)        
+        self.pos_map = {
+            "動": "V", "名": "N", "副": "D"
+        }
+    
+    def load_moe_data(self, fpath):
+        try:
+            with open(fpath, "r", encoding="UTF-8") as fin:
+                raw_data = json.load(fin)
+            
+            # index data
+            for data_x in raw_data:
+                title = data_x.get("title")
+                if not title: continue
+                if title.startswith("{"): continue
+                self.moe_data[title] = data_x
+        except Exception as ex:
+            print(ex)
+
+    def query_senses(self, word):        
+        moe_entry = self.moe_data.get(word, None)
+        heteronyms = moe_entry.get("heteronyms", [])
+        sense_iter = chain.from_iterable([x.get("definitions", None) for x in heteronyms])
+        sense_data = [x for x in sense_iter if x]
+        ret = []
+        for sense_x in sense_data:
+            sense_obj = {
+                "pos": self.pos_map.get(sense_x.get("type")),
+                "definition": sense_x.get("def", ""),
+                "example": sense_x.get("example", []),
+                "quote": sense_x.get("quote", [])                
+            }
+            ret.append(sense_obj)
+        return ret
+
+def query_word_pos(word, moe_inst):
+    return [query_char_pos(w, moe_inst) for w in word]
+
+def query_char_pos(char, moe_inst):
+    moe_senses = moe_inst.query_senses(char)
+    pos_counter = {"N": 0, "V": 0}
+    pos_freq = {"N": 0, "V": 0}
+    first_pos = ""
+
+    ## iterate over moe_senses
+    for sense_x in moe_senses:
+        sense_pos = sense_x["pos"]
+
+        ## only accept POS of N/V
+        if sense_pos not in ("N", "V"):
+            continue        
+
+        ## check if it is a classic sense. ignore the classic sense and continue to next
+        is_classic_sense = parse.is_classic(sense_x.get("quote", ""))
+        if is_classic_sense:
+            continue
+
+        ## set the first_pos if not yet set
+        if not first_pos:
+            first_pos = sense_pos
+
+        ## retrieve examplar words to calculate word frequency
+        examplar_words = parse.get_examplar_words(sense_x.get("example", ""))
+        word_freqs = [parse.query_as4_freq(w) for w in examplar_words]        
+
+        ## store the results: examplar words frequency and increment the POS count
+        pos_freq[sense_pos] += sum(word_freqs)
+        pos_counter[sense_pos] = pos_counter[sense_pos] + 1
+
+    ## Compare pos_counter to determine POS. If tied, then compare pos_freq
+    if pos_counter["N"] == pos_counter["V"]:
+        # tie condition, compare pos_freq
+        if pos_freq["N"] > pos_freq["V"]:
+            return "N"
+        elif pos_freq["N"] > pos_freq["V"]:
+            return "V"
+        elif first_pos:
+            return first_pos
+        else:
+            return None
+
+    elif pos_counter["N"] > pos_counter["V"]:
+        return "N"
+    else:
+        return "V"
+        
+    
